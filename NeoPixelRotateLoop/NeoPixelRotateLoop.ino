0ddae190d8da631a5915b81d5252723eb2ebdcae
Julian Renz <code@renz.cloud>
2019-08-23 23:03:43 +0200

    Improved and smoother fading. Not as funny as the old code but cleaner

--- NeoPixelRotateLoop/NeoPixelRotateLoop.ino
+++ NeoPixelRotateLoop/NeoPixelRotateLoop.ino
@@ -13,8 +13,10 @@
 const uint16_t PixelCount = 111; // make sure to set this to the number of pixels in your strip
 const uint16_t PixelPin = 2;  // make sure to set this to the correct pin, ignored for Esp8266
 const uint16_t AnimCount = 1; // we only need one
-const uint16_t TailLength = 105; // length of the tail, must be shorter than PixelCount
+const uint16_t TailLength = 55; // length of the tail, must be shorter than PixelCount
 const float MaxLightness = 0.5f; // max lightness at the head of the tail (0.5f is full bright)
+const float MinLightness = 0.15f;
+const float hue = 0.3f;
 
 NeoGamma<NeoGammaTableMethod> colorGamma; // for any fade animations, best to correct gamma
 
@@ -22,26 +24,15 @@ NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
 // for esp8266 omit the pinan
 //NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount);
 
+struct MyAnimationState
+{
+    RgbColor StartingColor;
+    RgbColor EndingColor;
+    uint16_t IndexPixel; // which pixel this animation is effecting
+};
+
 NeoPixelAnimator animations(AnimCount); // NeoPixel animation management object
 
-void SetRandomSeed()
-{
-    uint32_t seed;
-
-    // random works best with a seed that can use 31 bits
-    // analogRead on a unconnected pin tends toward less than four bits
-    seed = analogRead(0);
-    delay(1);
-
-    for (int shifts = 3; shifts < 31; shifts += 3)
-    {
-        seed ^= analogRead(0) << shifts;
-        delay(1);
-    }
-
-    // Serial.println(seed);
-    randomSeed(seed);
-}
 
 void LoopAnimUpdate(const AnimationParam& param)
 {
@@ -49,52 +40,85 @@ void LoopAnimUpdate(const AnimationParam& param)
     // we are using it as a timer of sorts
     if (param.state == AnimationState_Completed)
     {
+
+        
         // done, time to restart this position tracking animation/timer
         animations.RestartAnimation(param.index);
 
         // rotate the complete strip one pixel to the right on every update
         strip.RotateRight(1);
 
-        for (uint16_t index = 1; index <= strip.PixelCount()/* && index <= TailLength*/; index++)
-        {
-          HslColor oldColor = strip.GetPixelColor(index);
-          float newHue = oldColor.H + 0.01;
-          if(newHue > 1.0f) newHue = 0.0f;
-          HslColor newColor = HslColor(newHue, oldColor.S, oldColor.L);
-          strip.SetPixelColor(index, newColor);
-        }
+        
     }
 }
 
+/*
+void BlendAnimUpdate(const AnimationParam& param)
+{
+
+    if (param.state == AnimationState_Completed)
+    {
+        for (uint16_t index = 0; index < strip.PixelCount(); index++)
+        {
+          HslColor oldColor = strip.GetPixelColor(index);
+          float newHue = oldColor.H + 0.001f;
+          if(newHue > 1.0f) newHue = 0.0f;
+          RgbColor newColor = HslColor(newHue, oldColor.S, oldColor.L);
+          strip.SetPixelColor(index, colorGamma.Correct(newColor));
+        }      
+
+        // done, time to restart this position tracking animation/timer
+        animations.RestartAnimation(param.index);
+    }
+        
+}*/
+
 void DrawTailPixels()
 {
-    // using Hsl as it makes it easy to pick from similiar saturated colors
-    //float hue = random(360) / 360.0f;
+    
     float hue = 0.3f;
-    for (uint16_t index = 1; index <= strip.PixelCount()/* && index <= TailLength*/; index++)
+    for (uint16_t index = 0; index <= strip.PixelCount() / 2/*&& index <= TailLength*/; index++)
     {
-        //Serial.println("%i", index);
         float lightness = index * MaxLightness / TailLength;
-        if (lightness < 0.3) lightness = 0.3f;
+        if (lightness < MinLightness) lightness = MinLightness;
+        if (lightness > MaxLightness) lightness = MaxLightness;
         RgbColor color = HslColor(hue, 1.0f, lightness);
 
         strip.SetPixelColor(index, colorGamma.Correct(color));
+        strip.SetPixelColor(strip.PixelCount()-index, colorGamma.Correct(color));
+    }
+
+    for (uint16_t index = 0; index <= strip.PixelCount(); index++)
+    { 
+        HslColor color = strip.GetPixelColor(index);
+        /*if(color.L < MinLightness)
+        {
+          color.L = MinLightness;
+          RgbColor newColor = RgbColor(color);
+          strip.SetPixelColor(index, colorGamma.Correct(newColor));
+        }*/
+        
+        /*Serial.print(index);
+        Serial.print(":");
+        Serial.print(color.L);
+        Serial.println();*/
     }
 }
 
 void setup()
 {
-    //Serial.Begin(115200);
+    //Serial.begin(115200);
     strip.Begin();
     strip.Show();
 
-    SetRandomSeed();
+    //SetRandomSeed();
 
     // Draw the tail that will be rotated through all the rest of the pixels
     DrawTailPixels();
 
     
-    animations.StartAnimation(0, 30, LoopAnimUpdate); 
+    animations.StartAnimation(0, 25, LoopAnimUpdate); 
+    //animations.StartAnimation(1, 30, BlendAnimUpdate);
 }
 
 
